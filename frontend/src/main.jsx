45ffd6c74a49abf608e14dac0fffa95e089fdef5
Vhmed <vhmdhoss@gmail.com>
2025-05-20 19:15:22 +0300

    render app ,and starting vite configs

---
+++ frontend/src/main.jsx
@@ -0,0 +1,9 @@
+import "./global.css";
+import "./Theme.css";
+import "./Layers.css";
+import "./Languages/i18next.js";
+import "@splidejs/react-splide/css";
+
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+createRoot(document.getElementById("letstopic")).render(<App />);
