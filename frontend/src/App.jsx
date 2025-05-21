8f56b6ee6616206327369caa0b5995ea330d66c7
Vhmed <vhmdhoss@gmail.com>
2025-05-21 20:40:13 +0300

    adding connection handler in app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
 import { RouterProvider } from "react-router-dom";
 import letstopic from "./Routing/Letstopic";
 import { Toaster } from "react-hot-toast";
+import useNetworkStatus from "./connection/handleNetwork";
+import NetworkStatusView from "./Connection/Offline Page/OfflinePage";
 export default function App() {
+  const isOnline = useNetworkStatus();
+  if (!isOnline) {
+    return (
+      <>
+        <NetworkStatusView />
+      </>
+    );
+  }
   return (
     <>
       <div>
