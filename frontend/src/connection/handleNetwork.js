824a9073382c4674a71c8085e4018e1fce766ad4
Vhmed <vhmdhoss@gmail.com>
2025-05-25 18:20:23 +0300

    adding socket connection

--- frontend/src/connection/handleNetwork.js
+++ frontend/src/connection/handleNetwork.js
@@ -1,15 +1,17 @@
 import { useEffect, useState } from "react";
 import { SuccesToast } from "../components/Toast/Toasters";
+import { useAuthStore } from "../store/useAuthStore";
 
 export default function useNetworkStatus() {
   const [isOnline, setIsOnline] = useState(navigator.onLine);
-
+  const {connectSocket} =useAuthStore()
   useEffect(() => {
     const updateNetwork = () => {
       const online = navigator.onLine;
       setIsOnline(online);
       if (online) {
-        SuccesToast("You are online , please refresh the page");
+        SuccesToast("You are online!");
+        connectSocket()
       }
     };
 
