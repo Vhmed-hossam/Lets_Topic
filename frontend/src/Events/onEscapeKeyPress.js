a4a752402904e2c0e1490a8d32453e6c581101b1
Vhmed <vhmdhoss@gmail.com>
2025-05-26 22:09:09 +0300

    adding popovers closing while escape key is clicked

--- frontend/src/Events/onEscapeKeyPress.js
+++ frontend/src/Events/onEscapeKeyPress.js
@@ -1,11 +1,26 @@
 import { useChatStore } from "../store/useChatStore";
+import { usePopoversStore } from "../store/usePopoversStore";
 export default function onEscapeKeyPress() {
   const { SelectedUser, SetSelectedUser } = useChatStore();
+  const {
+    CloseReportPopover,
+    CloseUnfriendPopover,
+    CloseWipePopover,
+    CloseBlockPopover,
+    CloseEditMessagePopover,
+    CloseDeleteMessagePopover,
+  } = usePopoversStore();
   document.addEventListener("keydown", (event) => {
     if (event.key === "Escape") {
       if (SelectedUser?._id) {
         SetSelectedUser(null);
       }
+      CloseReportPopover();
+      CloseUnfriendPopover();
+      CloseWipePopover();
+      CloseBlockPopover();
+      CloseEditMessagePopover();
+      CloseDeleteMessagePopover();
       document.activeElement.blur();
     }
   });
