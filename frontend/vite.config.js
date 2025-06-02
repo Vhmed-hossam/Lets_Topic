df9303d9619349c9df5044c57e646eb12a540bfb
Vhmed <vhmdhoss@gmail.com>
2025-06-03 00:38:22 +0300

    fixing deployments errors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,20 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react")) return "react-vendor";
+            return "vendor";
+          }
+        },
+      },
+    },
+    outDir: "dist",
+    emptyOutDir: true,
+  },
 });
