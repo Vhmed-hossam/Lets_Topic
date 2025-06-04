ae1a99fd27cf78801ca6213d436cd74c51fd207d
Vhmed <vhmdhoss@gmail.com>
2025-06-04 04:21:03 +0300

    updating BASEURL for socket client issues

--- frontend/src/Data/BASE_URL.js
+++ frontend/src/Data/BASE_URL.js
@@ -1,3 +1,5 @@
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5001" : "/api";
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001"
+    : "https://letstopic.up.railway.app";
 export default BASE_URL;
