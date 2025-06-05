07476a137dfb28d70afe10bf14524c4ffaca6da9
Vhmed <vhmdhoss@gmail.com>
2025-06-05 17:26:24 +0300

    REMOVING unused api functions

--- backend/src/controllers/authcontroller.js
+++ backend/src/controllers/authcontroller.js
@@ -81,37 +81,6 @@ export const signup = async (req, res) => {
   }
 };
 
-export const verifyCode = async (req, res) => {
-  const { email, code } = req.body;
-  try {
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(404).json({ error: "User not found" });
-    }
-
-    if (
-      user.codeAuthentication !== code ||
-      user.codeType !== "email_verification"
-    ) {
-      return res.status(400).json({ error: "Invalid authentication code" });
-    }
-    if (user.codeAuthenticationExpires < new Date()) {
-      return res.status(400).json({ error: "Verification code expired" });
-    }
-
-    user.isVerified = true;
-    user.codeAuthentication = null;
-    user.codeAuthenticationExpires = null;
-    user.codeType = null;
-    await user.save();
-
-    res.status(200).json({ message: "Code verified successfully" });
-  } catch (error) {
-    console.error("VerifyCode error:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-};
-
 export const verifyEmail = async (req, res) => {
   const { email, code } = req.body;
   try {
@@ -296,20 +265,6 @@ export const verifyToken = (req, res) => {
   } catch (error) {
     console.error("Token verification error:", error);
     return res.status(401).json({ error: "Invalid or expired token" });
-  }
-};
-
-export const getUserById = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const user = await User.findById(id).select("-password");
-    if (!user) {
-      return res.status(404).json({ error: "User not found" });
-    }
-    res.status(200).json({ user });
-  } catch (error) {
-    console.error("GetUserById error:", error);
-    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
