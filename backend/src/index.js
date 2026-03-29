72c416557ef502d5ae934b4cbbd55511e4b1d56e
Vhmed <vhmdhoss@gmail.com>
2026-03-29 02:33:15 +0200

    moving config line above all the code

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,12 +10,13 @@ import OwnerRoutes from "./Owner/owner.route.js";
 import MessageRoutes from "./routes/message.route.js";
 import FriendRoutes from "./routes/friends.route.js";
 import WipeChatRoutes from "./routes/wipchat.route.js";
-export const BaseUrl =
-  process.env.NODE_ENV == "development"
-    ? "http://localhost:5173"
-    : process.env.FRONTEND_URL
-
 dotenv.config();
+export const BaseUrl =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5173"
+    : process.env.FRONTEND_URL;
+
+
 app.use(
   cors({
     origin: BaseUrl,
