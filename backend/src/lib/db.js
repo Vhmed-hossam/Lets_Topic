b58922babf54f163151505e95109f4babba0765c
Vhmed <vhmdhoss@gmail.com>
2026-08-07 07:41:56 +0300

    testing mongodb

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,8 +1,10 @@
-import mongoose from "mongoose";
-
 export const connectDB = async () => {
   try {
+    console.log("Mongo URL exists:", !!process.env.MONGODB_URL);
+
     const connection = await mongoose.connect(process.env.MONGODB_URL);
+
+    console.log("Connected to MongoDB");
   } catch (error) {
     console.log(error);
   }
